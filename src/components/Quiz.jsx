af27568cfcf9d2ac9ac590d0b34ba23b034b44ec
Md Rakibul Hasan <hello.rakibh@gmail.com>
2024-09-18 20:11:40 +0600

    Added quiz

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -3,7 +3,7 @@ const Quiz = () => {
     <div className="quiz">
       <div className="question">Who is the GOAT Footballer?</div>
       <div className="answers">
-        <div className="answer">LM10</div>
+        <div className="answer wrong">LM10</div>
         <div className="answer">LM10</div>
         <div className="answer">LM10</div>
         <div className="answer">LM10</div>
