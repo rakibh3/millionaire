5e450a6c63a59efe754394001262f125fc65a62b
Md Rakibul Hasan <hello.rakibh@gmail.com>
2024-09-18 19:27:57 +0600

    add money to board

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,14 @@ function App() {
   return (
     <div className="app">
       <div className="main">Main</div>
-      <div className="pyramid">Money</div>
+      <div className="pyramid">
+        <ul className="monetList">
+          <li className="moneyListItem">$ 100</li>
+          <li className="moneyListItem">$ 200</li>
+          <li className="moneyListItem">$ 300</li>
+          <li className="moneyListItem">$ 400</li>
+        </ul>
+      </div>
     </div>
   );
 }
